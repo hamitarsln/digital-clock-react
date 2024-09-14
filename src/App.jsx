6d092acfc79f1a612bbb52c2c5bf108af8356f73
Hamit Tuğrul Arslan <98461887+hamitarsln@users.noreply.github.com>
2024-09-14 19:17:58 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// A react app to display a digital clock. Built with the help of useState and useEffect hooks.
+
+import DigitalClock from './DigitalClock'
+
+function App() {
+
+  return (
+    <>
+      <DigitalClock />
+    </>
+  )
+}
+
+export default App
